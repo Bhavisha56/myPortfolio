5dd47fb17dee40d6423c6358d6619b0bd8a96c23
Bhavisha56 <97666630+Bhavisha56@users.noreply.github.com>
2024-07-15 10:54:41 +0530

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,33 @@
+function togglemenu() {
+    const menu = document.querySelector(".menulinks");
+    const icon = document.querySelector(".iconmenu");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+var crsr = document.querySelector("#cursor");
+var blur = document.querySelector("#cursor-blur");
+
+document.addEventListener("mousemove", function (dets) {
+  crsr.style.left = dets.x + "px";
+  crsr.style.top = dets.y + "px";
+  blur.style.left = dets.x - 250 + "px";
+  blur.style.top = dets.y - 250+ "px";
+});
+
+var h4all = document.querySelectorAll("#nav h4");
+h4all.forEach(function (elem) {
+  elem.addEventListener("mouseenter", function () {
+    crsr.style.scale = 3;
+    crsr.style.border = "1px solid #fff";
+    crsr.style.backgroundColor = "transparent";
+  });
+  elem.addEventListener("mouseleave", function () {
+    crsr.style.scale = 1;
+    crsr.style.border = "0px solid #95C11E";
+    crsr.style.backgroundColor = "#95C11E";
+  });
+});
+const toggleButton = document.getElementById('mode-toggle');
+toggleButton.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+});
